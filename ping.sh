4a33a600dcbedfc3987b29383d8edaeda9a38b68
w243420707 <yooyu@msn.com>
2024-02-19 02:30:17 +0800

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -6,7 +6,7 @@ ip_address=$(curl -s ifconfig.me)
 # 执行ping命令，并检查结果
 if ping -c 5 -W 2 -i 0.2 119.167.219.185 | grep "100% packet loss" > /dev/null
 then
-    echo "❌"
+    echo "⛔⛔⛔"
 else
-    echo "✔️"
+    echo "✅✅✅"
 fi
