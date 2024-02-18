eae097ead02f73e7b13001f8518a66faac47cfb4
w243420707 <yooyu@msn.com>
2024-02-19 02:17:26 +0800

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -4,9 +4,9 @@
 ip_address=$(curl -s ifconfig.me)
  
 # 执行ping命令，并检查结果
-if ping -c 5 -W 2 -i 0.2 www.itdog.cn | grep "100% packet loss" > /dev/null
+if ping -c 5 -W 2 -i 0.2 119.167.219.185 | grep "100% packet loss" > /dev/null
 then
-    echo "当前IP已经被封锁"
+    echo "****当前IP已经被封锁****"
 else
     echo "当前IP未被封锁"
 fi
